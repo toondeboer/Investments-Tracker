9a595431d3e9ea8e12d1e47366e2d3478315e306
Toon de Boer <toondeboer98@icloud.com>
2025-01-15 18:28:18 +0100

    test new api gateway

--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   yahooLambdaUrl:
     'https://42zobgj5c7.execute-api.us-east-1.amazonaws.com/default/yahoo_finance',
   dynamoDBLambdaUrl:
-    'https://42zobgj5c7.execute-api.us-east-1.amazonaws.com/default/microservice',
+    'https://a19ewvryna.execute-api.us-east-1.amazonaws.com/prod/microservice',
 };
