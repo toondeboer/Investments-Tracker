2513f91346cb63413baf9343efffdd6f9fb118a9
Toon de Boer <toondeboer98@icloud.com>
2025-01-16 08:42:18 +0100

    Update JwtInterceptor.ts

--- apps/frontend/src/auth/jwtInterceptor/JwtInterceptor.ts
+++ apps/frontend/src/auth/jwtInterceptor/JwtInterceptor.ts
@@ -17,7 +17,7 @@ export class JwtInterceptor implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    return this.oidcSecurityService.geIdToken().pipe(
+    return this.oidcSecurityService.getIdToken().pipe(
       switchMap((token: string) => {
         if (token) {
           req = req.clone({
