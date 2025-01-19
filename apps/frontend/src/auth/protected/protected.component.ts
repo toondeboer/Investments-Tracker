e51f016238f11f78e827385bf91cac91e22856a3
Toon de Boer <toondeboer98@icloud.com>
2025-01-19 10:30:38 +0100

    landing page

--- apps/frontend/src/auth/protected/protected.component.ts
+++ apps/frontend/src/auth/protected/protected.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, Inject, inject, OnInit } from '@angular/core';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { Router } from '@angular/router';
+import { LandingPageComponent } from '../../../../../libs/frontend/ui/src/lib/landing-page.component';
 
 @Component({
   selector: 'aws-protected',
   templateUrl: './protected.component.html',
   styleUrls: ['./protected.component.scss'],
-  imports: [CommonModule],
+  imports: [CommonModule, LandingPageComponent],
 })
 export class ProtectedComponent implements OnInit {
   constructor(
@@ -19,29 +20,7 @@ export class ProtectedComponent implements OnInit {
 
   isAuthenticated = false;
 
-  title = 'Investment Tracker';
-  description =
-    'Track your investments, monitor portfolio value, and stay on top of your financial goals with ease.';
-  features = [
-    {
-      title: 'Real-Time Tracking',
-      description:
-        'Get updates on your portfolio value and performance in real-time.',
-    },
-    {
-      title: 'Investment History',
-      description:
-        'Review the history of your investments and track their growth.',
-    },
-    {
-      title: 'Financial Insights',
-      description:
-        'Get personalized insights and recommendations based on your investment patterns.',
-    },
-  ];
-
   ngOnInit(): void {
-    console.log(this.environment);
     this.oidcSecurityService.checkAuth().subscribe(({ isAuthenticated }) => {
       this.isAuthenticated = isAuthenticated;
 
---
+++ libs/frontend/ui/src/lib/landing-page.component.ts
@@ -0,0 +1,73 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChartComponent } from './chart/chart.component';
+
+@Component({
+  selector: 'aws-landing-page',
+  standalone: true,
+  imports: [CommonModule, ChartComponent],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss',
+})
+export class LandingPageComponent implements OnInit {
+  @Output() login = new EventEmitter();
+
+  features = [
+    {
+      title: 'Real-time Analytics',
+      description:
+        'Get instant insights into your portfolio performance with advanced analytics tools.',
+    },
+    {
+      title: 'Global Markets',
+      description:
+        'Track investments across multiple markets and currencies in one place.',
+    },
+    {
+      title: 'Bank-grade Security',
+      description:
+        'Your data is protected with enterprise-level security and encryption.',
+    },
+  ];
+
+  chartData = {
+    x: [new Date()],
+    y: [1],
+  };
+
+  chartDat = [
+    { month: 'Jan', value: 1000 },
+    { month: 'Feb', value: 1200 },
+    { month: 'Mar', value: 1100 },
+    { month: 'Apr', value: 1400 },
+    { month: 'May', value: 1800 },
+    { month: 'Jun', value: 2200 },
+  ];
+
+  onClick() {
+    this.login.emit();
+  }
+
+  ngOnInit(): void {
+    const dates = [];
+    const values = [];
+    const today = new Date();
+    const min = 100;
+    const max = 1000;
+    let currentValue = Math.floor(Math.random() * (max - min) + min);
+
+    for (let i = 12; i > 0; i--) {
+      const firstDay = new Date(today.getFullYear(), today.getMonth() - i, 1);
+      dates.push(firstDay);
+
+      // Generate a small random step, allowing some backward steps but favoring upward.
+      const step = Math.floor(Math.random() * 200 - 50); // Range: -50 to +150
+      currentValue = currentValue + step;
+      values.push(currentValue);
+    }
+    this.chartData = {
+      x: dates,
+      y: values,
+    };
+  }
+}
